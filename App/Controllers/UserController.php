599fd4f01c7fd0799a43041ae20266f96bf9b14c
sharkfoolish <rich.pyps@gmail.com>
2023-01-02 22:55:23 +0800

    add retrieveAssignedDiningTable() in UserController.php

---
+++ App/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Assignment;
+use App\Models\DiningTable;
+use App\Models\Order;
+
+class UserController extends Controller {
+
+    public static function retrieveAssignedDiningTable(){
+        $assignmentTables = [];
+        $assignments = Assignment::where('designee_id', $_SESSION['id'], 'is_finished', 0);
+        foreach($assignments as $index => $assignment) {
+            $assignmentTables[$index] = DiningTable::where('id', $assignment['dining_table_id'])[0];
+            $assignmentTables[$index]['orders'] = Order::loadDish(Order::where('dining_table_id', $assignment['dining_table_id']));
+        }
+        UserController::response($assignmentTables);
+    }
+
+}
---
+++ App/Models/DiningTable.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+
+class DiningTable extends Model
+{
+    static string $name = 'dining_tables';
+}
